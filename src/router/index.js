d5a944c968d080873ee823f5f4ee2954d6cba19c
seeun9860 <san9860@naver.com>
2024-11-23 21:25:52 +0900

    마이페이지 최최종

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import SignUpView from '../views/SignUpView.vue';
 import MainView from '../views/MainView.vue';
 import NewPostView from '../views/NewPostView.vue';
 import VisitingPostView from '../views/VisitingPostView.vue';
+import MyPageView from '../views/MyPageView.vue';
 
 const routes = [
   { path: '/', name: 'Login', component: LoginView },
@@ -11,6 +12,7 @@ const routes = [
   { path: '/main', name: 'Main', component: MainView },
   { path: '/newpost', name: 'NewPost', component: NewPostView },
   { path: '/visiting/:id', name: 'Visiting', component: VisitingPostView },
+  { path: '/mypage', name: 'MyPage', component: MyPageView }
 ];
 
 const router = createRouter({
