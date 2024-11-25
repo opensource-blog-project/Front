64aa45ad607f027a180ea1db0ce9f34a36c73eef
hyunj24 <hyunjug7@naver.com>
2024-11-26 00:20:31 +0900

    마이페이지 구현

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import SignUpView from '../views/SignUpView.vue';
 import MainView from '../views/MainView.vue';
 import NewPostView from '../views/NewPostView.vue';
 import VisitingPostView from '../views/VisitingPostView.vue';
+import MyPageView from "@/views/MyPageView.vue";
 
 const routes = [
   { path: '/', name: 'Login', component: LoginView },
@@ -17,6 +18,7 @@ const routes = [
     component: NewPostView,
     props: true, // 수정 모드에서 데이터를 전달
   },
+  { path: "/mypage", name: "MyPage", component : MyPageView }
 ];
 
 const router = createRouter({
